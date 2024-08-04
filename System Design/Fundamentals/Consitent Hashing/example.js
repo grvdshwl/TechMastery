183e4fee35f45c83ac49ca5d6ec1c453299ee1d6
Gaurav Deshwal <gauravdeshwal@Gauravs-MacBook-Air.local>
2024-08-04 12:34:56 +0530

    DSA

--- System Design/Fundamentals/Consitent Hashing/example.js
+++ System Design/Fundamentals/Consitent Hashing/example.js
@@ -3,39 +3,33 @@ function hashString(str) {
   let hash = 0;
   for (let i = 0; i < str.length; i++) {
     hash = (hash << 5) - hash + str.charCodeAt(i);
+    hash = hash & hash; // Convert to 32-bit integer
   }
   return Math.abs(hash);
 }
 
-// Class to represent the consistent hashing ring with virtual nodes
+// Class to represent the consistent hashing ring
 class ConsistentHashing {
-  constructor(virtualNodes = 3) {
-    this.virtualNodes = virtualNodes; // Number of virtual nodes per physical node
+  constructor() {
     this.ring = [];
-    this.nodes = new Map(); // To keep track of virtual nodes and their corresponding physical nodes
+    this.nodes = new Map(); // To keep track of nodes and their positions
   }
 
-  // Add a node to the ring with virtual nodes
+  // Add a node to the ring
   addNode(node) {
-    for (let i = 0; i < this.virtualNodes; i++) {
-      const vnodeId = `${node}#${i}`;
-      const position = hashString(vnodeId);
-      this.ring.push(position);
-      this.nodes.set(position, node);
-    }
+    const position = hashString(node);
+    this.ring.push(position);
+    this.nodes.set(position, node);
     this.ring.sort((a, b) => a - b); // Keep the ring sorted
   }
 
-  // Remove a node and its virtual nodes from the ring
+  // Remove a node from the ring
   removeNode(node) {
-    for (let i = 0; i < this.virtualNodes; i++) {
-      const vnodeId = `${node}#${i}`;
-      const position = hashString(vnodeId);
-      const index = this.ring.indexOf(position);
-      if (index !== -1) {
-        this.ring.splice(index, 1);
-        this.nodes.delete(position);
-      }
+    const position = hashString(node);
+    const index = this.ring.indexOf(position);
+    if (index !== -1) {
+      this.ring.splice(index, 1);
+      this.nodes.delete(position);
     }
   }
 
@@ -53,7 +47,7 @@ class ConsistentHashing {
 }
 
 // Example usage
-const ch = new ConsistentHashing(5); // Using 5 virtual nodes per physical node
+const ch = new ConsistentHashing();
 
 // Add nodes to the ring
 ch.addNode("NodeA");
@@ -72,11 +66,10 @@ console.log("Node for Key3:", ch.getNode("Key3"));
 
 // Add a new node
 ch.addNode("NodeD");
-console.log(
-  "Ring after adding NodeD:",
-  ch.ring.map((pos) => ch.nodes.get(pos))
-);
 
+console.log("Node for Key1:", ch.getNode("Key1"));
+console.log("Node for Key2:", ch.getNode("Key2"));
+console.log("Node for Key3:", ch.getNode("Key3"));
 // Remove a node
 ch.removeNode("NodeB");
 console.log(

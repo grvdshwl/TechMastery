9322c2129fdfe0d3c7d9ba5b752b387d723d3173
Gaurav Deshwal <gauravd@geekyants.com>
2024-08-24 21:47:33 +0530

    system design

--- System Design/article.js
+++ System Design/article.js
@@ -12,3 +12,12 @@
 //* https://newsletter.systemdesign.one/p/zoom-architecture
 //* https://newsletter.systemdesign.one/p/virtual-waiting-room
 //* https://systemdesign.one/url-shortening-system-design/
+//* https://ghumare64.medium.com/system-design-blogs-that-will-make-you-a-pro-f2f2d20401ca
+
+//* https://ghumare64.medium.com/system-design-blogs-that-will-make-you-a-pro-f2f2d20401ca
+
+//* https://workat.tech/system-design/article/best-engineering-blogs-articles-videos-system-design-tvwa05b8bzzr
+
+//* https://dev.to/karanpratapsingh/series/19332
+
+//*  https://systemdesign.medium.com/

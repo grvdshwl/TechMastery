3b77dbd9fb95f68bfec761f639acdfd34453994c
Gaurav Deshwal <gauravd@geekyants.com>
2024-08-28 09:53:43 +0530

    System Design:Spotify

---
+++ System Design/Decision Making/index4.js
@@ -0,0 +1,10 @@
+// why use s3 over realtional db ?
+
+// S3 is highly scalable, handling massive data volumes automatically.
+// S3 is cost-efficient with a pay-as-you-go pricing model, ideal for large data.
+// S3 offers high durability and availability with data replicated across multiple regions.
+// S3 is optimized for high-throughput, low-latency access to large objects.
+// S3 integrates seamlessly with AWS services for complex workflows.
+// S3 provides robust security, including encryption and access controls.
+// S3 is a managed service, reducing operational overhead like scaling and backups.
+// S3 supports versioning and lifecycle policies for efficient data management.
--- System Design/Problems/Medium/Spotify/index.js
+++ System Design/Problems/Medium/Spotify/index.js
@@ -1,1 +1,65 @@
 //* https://www.youtube.com/watch?v=_K-eupuDVEc
+
+//* https://app.diagrams.net/#G1-cUKYUOoPfiPDEFZ8MDD1mAH1hckTQ_p#%7B%22pageId%22%3A%22U5GKEaanNevp4ycoBe4L%22%7D
+
+//* Functional Requirements
+//* Artists can upload song
+//* Users can search for a song add it to a playlist
+//* Users can stream a song.
+
+//* Non-Functional Requirements
+//* Scalability
+//* Low Latency (Performance)
+//* High Availability
+//* Data Consistency
+
+//* Capacity Estimation
+//* 200M million users
+//* 10M million monthly active users
+
+//* Storage Estimation
+//* 100M songs, each approximately 5 MB in size
+//* Total storage for 100M songs: 100M * 5 MB = 500 TB
+//* Replication across 7 CDNs
+//* Total replicated storage: 500 TB * 7 = 3.5 PB
+
+//* Storage Estimation for Daily Uploads
+//* 10,000 songs uploaded per day
+//* Each song is approximately 5 MB in size
+//* Daily storage for new uploads: 10,000 * 5 MB = 50 GB
+//* Replication across 7 CDNs
+//* Total replicated storage for daily uploads: 50 GB * 7 = 350 GB
+//*
+
+let newUser = {
+  username: "",
+  email: "",
+  password: "",
+  fullName: "",
+  dateOfBirth: "",
+  gender: "",
+  location: "",
+};
+
+let newPost = {
+  userId: "",
+  content: "",
+  media: {
+    trackId: "",
+    albumId: "",
+    artistId: "",
+    duration: "",
+  },
+  location: "",
+};
+
+const songData = {
+  songId: "",
+  songURL: "",
+  artistId: "",
+  duartion: "",
+  genre: "",
+  albumId: "",
+  thumbnail: "",
+  songName: "",
+};

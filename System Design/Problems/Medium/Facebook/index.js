5dcd7a2f152b39b8a78a984be2cfff3260cf0d02
Gaurav Deshwal <gauravd@geekyants.com>
2024-08-30 10:22:14 +0530

    System Design:Book my show

--- System Design/Problems/Medium/Facebook/index.js
+++ System Design/Problems/Medium/Facebook/index.js
@@ -1,53 +1,55 @@
-//* https://www.youtube.com/watch?v=9-hjBGxuiEs
-//* https://app.diagrams.net/#G1ZkpHRxHjVNuoTCLg-1dVoMOWfEyhrItq#%7B%22pageId%22%3A%22dGVP3WR8ZVDCpPc7mLXl%22%7D
-
 //* Functional Requirements
+//* https://app.diagrams.net/#G1BGHmmPcq9qupu9VgEbNXIELYuaVqLCdi#%7B%22pageId%22%3A%22zWcUh6njZ5WM0uNPp3_M%22%7D
 
-//* User Profile
-//* Users can Search Other Users
-//* Users can send Befriend, Unfriend or Block other users
-//* Post a message,image or video
-//* Messenger
-//* Feeds
+//* Browse Movies and Shows
+//* Search for Movies, Shows, and Events
+//* Book Tickets for Movies, Shows, and Events
+//* Notification for ticket booking status.
 
-//* Users
-//* Famous
-//* active
-//* live
-//* passive
-//* inactive
-
-//* Non Functional Requirements
-//* Scale
-//* Low Latency(Perfomance)
-//* Availibilty
+//* Non-Functional Requirements
+//* Scalability
+//* Low Latency (Performance)
+//* High Availability
+//* Security (Payment and Personal Data)
+//* Reliability
 
 //* Capacity Estimation
-//* 100M Million users
+//* 100M total users
+//* 30M monthly active users
+//* 10M daily searches
+//* 5M daily bookings
+//* 5M reviews per month
 
-//* 50M Million monthly
+//* We will use a RelationalDB for storing fairly static data such as the number of theaters, halls, or seats in theaters, and shard them by geolocation.
 
-//* 10M (2 posts a day)
-
-//* 100,00,000 * 2 * 50 kb = 100,00,00,000 = 1TB Per day storage.
-
-//* 100,000 New registration daily
-
-//*
+//* We will use a NoSQLDB like Cassandra for storing dynamic data such as movies data, shows data, reviews, and artist information, as this data will not always be consistent.
 
 let newUser = {
   mobileNumber: "",
   email: "",
   fullName: "",
-  gender: "",
+  password: "",
   dob: "",
   location: "",
 };
 
-let newPost = {
+let newBooking = {
   userId: "",
-  type: "",
-  postMessage: "",
-  image: {},
-  location: "",
+  movieId: "",
+  showId: "",
+  eventId: "",
+  bookingTime: "",
+  numberOfTickets: "",
+  paymentDetails: {},
+  bookingStatus: "",
+};
+
+let newReview = {
+  userId: "",
+  movieId: "",
+  showId: "",
+  eventId: "",
+  rating: "",
+  reviewMessage: "",
+  reviewDate: "",
 };

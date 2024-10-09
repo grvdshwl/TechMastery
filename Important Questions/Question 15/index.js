a7509e4d50ca6feee3ba34892b386ebbcd011148
Gaurav Deshwal <gauravd@geekyants.com>
2024-10-09 20:02:41 +0530

    Important questions

---
+++ Important Questions/Question 15/index.js
@@ -0,0 +1,1 @@
+//* What are some common pitfalls when using TypeScript with React's useState hook and how do you avoid them?
---
+++ Important Questions/Question 16/index.js
@@ -0,0 +1,1 @@
+//* What method would you use to efficiently track and manage state in a large-scale, complex React application?

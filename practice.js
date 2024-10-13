023debebc363197a5404cccbc6f0b71650e6f30b
Gaurav Deshwal <gauravd@geekyants.com>
2024-10-13 13:05:11 +0530

    DSA

--- practice.js
+++ practice.js
@@ -1,1 +1,31 @@
 //* Multiply Strings
+
+//* Reverse a Linked List
+//* Reverse a Linked List II
+//* LRU Cache
+
+//* Detect Cycle in a Linked List
+//*Linked List Cycle II
+//* Merge Two Sorted Lists
+
+//* Merge K Sorted Lists
+//* Remove Nth Node From End Of List
+//* Reorder List
+
+//* Doubly LikedList
+//* Circular LinkedList
+//* remove value from linked list
+
+//* Add Two Numbers
+//*Remove Duplicates from Sorted List
+//* Rotate List
+//* Reverse Nodes In K Group
+
+//* Merge Intervals
+//* Non-overlapping Intervals
+//* Meeting Rooms
+//*  Meeting Rooms II
+
+//* Remove Covered Intervals
+//* Maximum Number of Non-overlapping Meetings
+//* Summary Ranges

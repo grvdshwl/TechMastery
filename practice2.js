dfceb9fc0a7d357ec7f34072c599eb59216083cb
Gaurav Deshwal <gauravd@geekyants.com>
2024-10-30 19:28:36 +0530

    Javascript : Interview Questions

--- practice2.js
+++ practice2.js
@@ -1,2 +1,8 @@
-//* Uber
-//* Google Search
+//* This Keyword
+//* Revison - call bind apply, and objects
+
+//* Promises -
+//* Revision  -  closure and currying
+
+//* Revision
+//* Functions, Hoisting , scope and event propogation

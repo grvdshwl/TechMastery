575893e44be7bd8596dc3f49e79fd9224920dfa5
Gaurav Deshwal <gauravd@geekyants.com>
2024-12-01 16:44:34 +0530

    Interview preparation

--- plan.js
+++ plan.js
@@ -1,7 +1,15 @@
-//* SQL
-//* Node JS
-//* Regex
-//* Docker and kubernetes
-//* Python
-//* Advance JS
-//* Advance CSS
+// ---------------------------------------------
+// | Topic                  | Number of Weeks |
+// ---------------------------------------------
+// | SQL                    | 3 weeks         |
+// | Node JS                | 4 weeks         |
+// | Regex                  | 1 week          |
+// | Docker and Kubernetes  | 2 weeks         |
+// | Python                 | 11 weeks        |
+// | Django                 | 2 weeks         |
+// | Fast APIs              | 2 weeks         |
+// | Advance JS             | 6 weeks         |
+// | Advance CSS            | 4 weeks         |
+// ---------------------------------------------
+// Total                    | 35 weeks        |
+// ---------------------------------------------

5a1e0d7e4e37ada8dbf69b4eb0dc9a546576b210
Gaurav <gaurav.deshwal@nabis.com>
2024-12-30 15:11:34 +0530

    Learning:Docker

--- plan.js
+++ plan.js
@@ -2,18 +2,18 @@
 // | Topic                  | Number of Weeks |
 // ---------------------------------------------
 // | Grapqh QL              | 3 weeks         |
-// | Docker                 | 1 week          |
-// | Kubernetes             | 1 week          |
-// | SQL                    | 3 weeks         |
 // | Regex                  | 1 week          |
+// | SQL                    | 3 weeks         |
 // | Nginx                  | 1 week          |
+// | Docker                 | 2 weeks          |
+// | Kubernetes             | 2 weeks          |
 // | Node JS                | 4 weeks         |
 // | Nest JS                | 3 weeks         |
-// | Python                 | 11 weeks        |
+// | Python                 | 12 weeks        |
 // | Django                 | 2 weeks         |
 // | Fast APIs              | 2 weeks         |
 // | Advance JS             | 6 weeks         |
 // | Advance CSS            | 4 weeks         |
 // ---------------------------------------------
-// Total                    | 35 weeks        |
+// Total                    | 40 weeks        |
 // ---------------------------------------------

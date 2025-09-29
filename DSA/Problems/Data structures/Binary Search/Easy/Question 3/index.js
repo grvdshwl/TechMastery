2433e6336f43578f04e8ba0a2431fbde2b21d2e2
grvdshwl <gaurav.deshwal@nabis.com>
2025-09-29 15:42:58 +0530

    DSA:Binary search

--- DSA/Problems/Data structures/Binary Search/Easy/Question 3/index.js
+++ DSA/Problems/Data structures/Binary Search/Easy/Question 3/index.js
@@ -1,14 +1,22 @@
 //* Arranging Coins
 
 var arrangeCoins = function (n) {
-  let sum = 0;
-  for (let i = 1; i <= n; i++) {
-    sum = sum + i;
-    if (sum === n) {
-      return i;
-    }
-    if (sum > n) {
-      return i - 1;
+  let left = 1,
+    right = n
+  let result = 0
+
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2)
+    let total = (mid * (mid + 1)) / 2
+
+    if (total === n) return mid
+    if (total < n) {
+      result = mid
+      left = mid + 1
+    } else {
+      right = mid - 1
     }
   }
-};
+
+  return result
+}
--- DSA/Problems/Data structures/Binary Search/Easy/Question 6/index.js
+++ DSA/Problems/Data structures/Binary Search/Easy/Question 6/index.js
@@ -1,18 +1,27 @@
-//* Sqrt(x) ---done
-
+/**
+ * @param {number} x
+ * @return {number}
+ */
 var mySqrt = function (x) {
-  if (x < 2) return x;
-  let left = 0,
-    right = x;
+  if (x < 2) return x // quick return for 0 and 1
+
+  let left = 1
+  let right = Math.floor(x / 2)
+  let result = 0
+
   while (left <= right) {
-    const mid = Math.floor((left + right) / 2);
-    if (mid * mid <= x && x < (mid + 1) * (mid + 1)) {
-      return mid;
-    } else if (mid * mid > x) {
-      right = mid - 1;
+    const mid = Math.floor((left + right) / 2)
+    const square = mid * mid
+
+    if (square === x) {
+      return mid
+    } else if (square < x) {
+      result = mid
+      left = mid + 1
     } else {
-      left = mid + 1;
+      right = mid - 1
     }
   }
-  return -1;
-};
+
+  return result
+}
